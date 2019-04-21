2be564a1e60b67f8a907f043f612c0741df066b3
mike <mikey12349876@gmail.com>
2019-04-21 05:09:04 -0400

    commit

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(3000, () => console.log("Server started..."));
