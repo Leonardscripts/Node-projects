622a078fad9af65fc382e9076bd091526196069d
mike <mikey12349876@gmail.com>
2019-04-21 05:20:39 -0400

    commit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+const path = require("path");
 const nodemailer = require("nodemailer");
 
 const app = express();
+
+// View engine setup
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+// Static folder
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
