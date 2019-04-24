860a301f03415480203d72e6d09198fbfcd94a25
mike <mikey12349876@gmail.com>
2019-04-24 06:31:22 -0400

    commit

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const exphbs = require("express-handlebars");
 const path = require("path");
 const nodemailer = require("nodemailer");
 
-
 const app = express();
-
-
 
 // View engine setup
 app.engine("handlebars", exphbs());
@@ -37,33 +34,40 @@ app.post("/send", (req, res) => {
  <h3>Message</h3>
  <p>${req.body.message}</p>
  `;
- // create reusable transporter object using the default SMTP transport
- let transporter = nodemailer.createTransport({
-  host: "main.mikemedia.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: 'test@mike.com', // generated ethereal user
-    pass: testAccount.pass // generated ethereal password
-  }
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "mikey12349876@gmail.com", // generated ethereal user
+      pass: "survivor1" // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Nodemailer Contact" <test@mike.com>', // sender address
+    to: "mikey12349876@gmail.com", // list of receivers
+    subject: "Node Contact Request", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message sent: %s", info.messageId);
+
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    res.render("contact", { msg: "Email has been sent" });
+  });
 });
-
-// send mail with defined transport object
-let info = await transporter.sendMail({
-  from: '"Fred Foo 👻" <foo@example.com>', // sender address
-  to: "bar@example.com, baz@example.com", // list of receivers
-  subject: "Hello ✔", // Subject line
-  text: "Hello world?", // plain text body
-  html: "<b>Hello world?</b>" // html body
-});
-
-console.log("Message sent: %s", info.messageId);
-// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-// Preview only available when sending through an Ethereal account
-console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-});
-
 
 app.listen(3000, () => console.log("Server started..."));
